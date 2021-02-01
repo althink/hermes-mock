70a0073fea17878b912b269d015d05b6987bb32d
SZCZESNIAK Krzysztof <krzysztof.szczesniak@idemia.com>
2021-02-01 10:06:46 -0300

    add stub for adding groups

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	rtr := mux.NewRouter()
 	rtr.HandleFunc("/topics/{topic:[a-z.]+}", postOnTopic).Methods("POST")
+	rtr.HandleFunc("/groups", postOnGroups).Methods("POST")
 	http.ListenAndServe(":8080", rtr)
 }
 
@@ -16,4 +17,8 @@ func postOnTopic(w http.ResponseWriter, req *http.Request) {
 	params := mux.Vars(req)
 	topic := params["topic"]
 	println("Handle POST on topic: " + topic)
+}
+
+func postOnGroups(w http.ResponseWriter, req *http.Request) {
+	println("Handle POST on groups")
 }
